c0716d217c577104300ad9b9bf9070a665236b86
sergi <sergioasenciorodriguez@gmail.com>
2023-05-19 08:19:38 -0500

    Archivos actualizados 2

--- src/main/java/views/Main.java
+++ src/main/java/views/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
             stage.setScene(scene);
             stage.show();
 
+
         } catch (Exception e){
             e.printStackTrace();
         }
