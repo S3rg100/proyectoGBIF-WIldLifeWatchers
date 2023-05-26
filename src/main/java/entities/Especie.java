24a552701ac4962610d99f583ddc5758a1dc4e47
S3rg100 <105468995+S3rg100@users.noreply.github.com>
2023-05-25 21:12:16 -0500

    Update Especie.java (WEBHOOK)

--- src/main/java/entities/Especie.java
+++ src/main/java/entities/Especie.java
@@ -4,21 +4,33 @@ public class Especie {
 
     private String Nombre_especie;
     private int id_Especie;
+    
+    
 
     public void crear_especie (String nombre_especie, int id_Especie){
         this.Nombre_especie = Nombre_especie;
         this.id_Especie = id_Especie;
+        
+        
     }
     public void setNombre_especie(String nombre_especie) {
         Nombre_especie = nombre_especie;
+        
+        
     }
     public String getNombre_especie() {
         return Nombre_especie;
+        
+        
     }
     public void setId_Especie(int id_Especie) {
         this.id_Especie = id_Especie;
+        
+        
     }
     public int getId_Especie() {
         return id_Especie;
+        
+        
     }
 }
